0ab0e3a8987019a43b2cf45fedffe54cb0622070
yyang12302 <100991505+yyang12302@users.noreply.github.com>
2024-07-01 13:39:13 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -9,7 +9,7 @@ def pin_to_ipfs(data):
 		'file':('data.json',json_data,'application/json')
 	}
 
-	gateway="https://api.pinata.cloud/pinning/pinFileToIPFS"
+	gateway=f"https://api.pinata.cloud/pinning/pinFileToIPFS"
 	headers={
 		'pinata_api_key':'6bf509970b4e9c3ecee6',
 		'pinata_secret_api_key':'d935f96f288a65d6fd2b5984af63efa47d6022cc03cba8aeebefd65a330fa9a6'
@@ -24,7 +24,7 @@ def pin_to_ipfs(data):
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
 	#YOUR CODE HERE	
-	gateway=f"https://api.pinata.cloud/pinning/pinFileToIPFS"
+	gateway=f"https://gateway.pinata.cloud/ipfs/{cid}"
 	headers={
 		'pinata_api_key':'6bf509970b4e9c3ecee6',
 		'pinata_secret_api_key':'d935f96f288a65d6fd2b5984af63efa47d6022cc03cba8aeebefd65a330fa9a6'
